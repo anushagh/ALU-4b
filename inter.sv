6ecf0693a1d82d8928dcacb725407f0638d531e3
anushagh <89003643+anushagh@users.noreply.github.com>
2022-07-21 20:08:23 +0530

    Create inter.sv

---
+++ inter.sv
@@ -0,0 +1,21 @@
+interface alu_inter( input bit clk);
+
+logic reset;
+
+logic [3:0] a,b;
+
+logic [2:0] opcode;
+
+logic [7:0] c;
+
+clocking cb@(posedge clk);
+
+default input #2ns output #3ns;
+
+output a, b, opcode;
+
+endclocking
+
+modport TB(clocking cb, output reset);
+
+endinterface
